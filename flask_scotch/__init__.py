a167ec8ed2c4bb157e6d713e25139dbcebd0aa4a
AzariasB <azarias.boutin@probance.com>
2021-11-11 16:55:07 +0100

    Version bump for internal testing

--- flask_scotch/__init__.py
+++ flask_scotch/__init__.py
@@ -7,7 +7,7 @@ from .RemoteRelationship import RemoteRelationship
 from .LocalRelationship import LocalRelationship
 from .LocalModel import LocalModel
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 class FlaskScotch:
